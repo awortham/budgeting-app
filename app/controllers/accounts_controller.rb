10646eb33e03c0fb532b04e87938a6748a08e006
awortham <wortham.aaron@gmail.com>
2015-01-19 14:34:15 -0700

    added some views and functionality

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,13 @@ class AccountsController < ApplicationController
   def new
     @account = Account.new
   end
+
+  def index
+    @accounts = current_user.accounts
+  end
+
+  def create
+
+    redirect_to accounts_path(current_user)
+  end
 end

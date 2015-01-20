656b1cd17487dc12eb42436b1f85afcc8ef21f9c
awortham <wortham.aaron@gmail.com>
2015-01-20 12:53:23 -0700

    Revert "merge conflicts"
    This reverts commit 27c7ac3527b48fb7b248d4594ec5834e5bd18ac5, reversing
    changes made to 66770c8596389203bd501d36c9fd99dcc1b39d3f.

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Dominick Sanchez'
+    email 'rockyou@sweet.com'
+    password 'Sanchez2'
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  before do
+    @user = create(:user)
+    @account = create(:account, user_id: 1)
+  end
+
+  it 'is valid' do
+    expect(@user).to be_valid
+  end
+
+  it 'has accounts associated' do
+    expect(@user.accounts.first).to eq @account
+  end
+end

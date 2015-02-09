778078ab33657098328816a3bf085704ab1edc04
awortham <wortham.aaron@gmail.com>
2015-02-09 13:37:16 -0700

    you can access dashboard if logged in

---
+++ spec/features/account_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'account actions' do
+  it 'cannot access account dashboard if not logged in' do
+    visit accounts_path
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'can access account dashboard if logged in' do
+    user = create(:user)
+    page.set_rack_session("warden.user.user.key" => User.serialize_into_session(user).unshift("User"))
+    visit accounts_path
+    expect(current_path).to eq accounts_path
+  end
+end

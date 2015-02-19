9391d9a1c0d5b474edc1cfabf430d078f7b45095
awortham <wortham.aaron@gmail.com>
2015-02-19 15:34:38 -0700

    balance on show page and changes to form

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -39,6 +39,6 @@ class AccountsController < ApplicationController
   private
 
   def account_params
-    params.require(:account).permit(:name)
+    params.require(:account).permit(:name, :balance)
   end
 end
